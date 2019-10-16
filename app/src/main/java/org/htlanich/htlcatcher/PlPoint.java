9bbae72ce15c220ffed357b0c70438674678b828
Bemoty <jos.winkler@tsn.at>
2019-10-16 08:42:47 +0200

    refactored and documented plpoint

--- app/src/main/java/org/htlanich/htlcatcher/PlPoint.java
+++ app/src/main/java/org/htlanich/htlcatcher/PlPoint.java
@@ -5,40 +5,50 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
+ * Represents a point in the HTLCatcher GameView
+ *
  * Created by albert on 06.11.17.
  */
-
 public class PlPoint extends Point {
 
-  public PlPoint(int x, int y) {
+  PlPoint(int x, int y) {
     super(x, y);
   }
 
   /**
+   * Checks if a provided list of points intersects with this point
+   *
+   * @param points a list of points which need to be checked for intersection
    * @param radius the radius each point has (including actual point)
-   * @return null, if no point intersects, otherwise the intersecting point
+   * @return null, if no point intersects, otherwise the intersecting points
    */
-  public List<PlPoint> intersect(List<PlPoint> points, double radius) {
-    List<PlPoint> ps = new ArrayList<>();
-    for (PlPoint p : points) {
-      if (intersect(p, radius)) {
-        ps.add(p);
+  @SuppressWarnings("SameParameterValue")
+  List<PlPoint> intersect(final List<PlPoint> points, final double radius) {
+    List<PlPoint> returnPoints = new ArrayList<>();
+    for (PlPoint point : points) {
+      if (intersect(point, radius)) {
+        returnPoints.add(point);
       }
     }
-    return ps;
+    return returnPoints;
   }
 
-  public boolean intersect(PlPoint p, double radius) {
-    double distance = Math
-        .pow((this.x - p.x) * (this.x - p.x) + (this.y - p.y) * (this.y - p.y), 0.5);
-    return (distance < radius);
+  /**
+   * Checks if this point intersects with another one
+   *
+   * @param point the point to be checked
+   * @param radius the radius of the point
+   * @return true if this point intersects with the provided point, false otherwise
+   */
+  private boolean intersect(PlPoint point, double radius) {
+    return
+        Math.pow((this.x - point.x) * (this.x - point.x) + (this.y - point.y) * (this.y - point.y),
+            0.5) < radius;
   }
 
   @Override
   public String toString() {
-    return "PlPoint{" +
-        "x=" + x +
-        ", y=" + y +
-        '}';
+    return "PlPoint{x=" + x + ", y=" + y + "}";
   }
+
 }
