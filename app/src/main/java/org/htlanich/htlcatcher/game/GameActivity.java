cd67e98ba5616f48ccde3edf73d6abd647f8924e
Bemoty <jos.winkler@tsn.at>
2019-10-23 10:41:22 +0200

    make game activity full screen

--- app/src/main/java/org/htlanich/htlcatcher/game/GameActivity.java
+++ app/src/main/java/org/htlanich/htlcatcher/game/GameActivity.java
@@ -11,6 +11,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import java.util.Timer;
 import java.util.TimerTask;
 import org.htlanich.htlcatcher.util.ViewPoint;
@@ -65,6 +67,10 @@ public class GameActivity extends AppCompatActivity implements SensorEventListen
         }
       }
     }, 0, 10);
+
+    // Set window fullscreen
+    this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+    this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
     // Register view
     setContentView(gameView);
