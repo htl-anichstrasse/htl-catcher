af8ec7654837ec6967c452cfd6b1b104898511b2
Bemoty <jos.winkler@tsn.at>
2020-01-08 14:19:06 +0100

    add frontend

--- views/resources/add.py
+++ views/resources/add.py
@@ -12,23 +12,23 @@ user_manager = UserManager(os.path.join(
     str(Path(os.path.dirname(os.path.abspath(__file__))).parents[1]), "users.json"))
 
 
-def check(authorization_header: str) -> bool:
-    """ Checks the authorization header using the user manager. Returns true
-     if the user + password in the header is valid, false otherwise.
-
-    Please note that the authorization must be encoded in Base64 as per RFC 2617"""
-
-    decoded_username_password = base64.b64decode(
-        authorization_header.split()[-1].encode('ascii')).decode('ascii')
-    username_password_split = decoded_username_password.split(':')
-    return user_manager.validate_user(username_password_split[0], username_password_split[1])
-
-
 class add(Resource):
+    def check(self, authorization_header: str) -> bool:
+        """ Checks the authorization header using the user manager. Returns true
+        if the user + password in the header is valid, false otherwise.
+
+        Please note that the authorization must be encoded in Base64 as per RFC 2617"""
+
+        decoded_username_password = base64.b64decode(
+            authorization_header.split()[-1].encode('ascii')).decode('ascii')
+        username_password_split = decoded_username_password.split(':')
+        return user_manager.validate_user(username_password_split[0], username_password_split[1])
+
     def post(self):
         authorization_header = request.headers.get('Authorization')
-        if authorization_header and check(authorization_header):
+        if authorization_header and self.check(authorization_header):
             return {'message': 'Authorized oida'}
 
         # authorization failed
-        return {'message': 'You need to be authorized to perform this action.'}, 401  # abort, user is not authenticated
+        # abort, user is not authenticated
+        return {'message': 'You need to be authorized to perform this action.'}, 401
