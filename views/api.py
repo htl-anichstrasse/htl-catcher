5230976218cfbd7969056c1eb0abff08c81ebf21
Bemoty <jos.winkler@tsn.at>
2020-01-09 11:33:48 +0100

    added leaderboard manager

--- views/api.py
+++ views/api.py
@@ -1,11 +1,17 @@
-from flask import Blueprint
+import os
+from pathlib import Path
 
+from flask import Blueprint
 from flask_restful import Api
 
+from ..leaderboard import LeaderboardManager
 from .resources.add import add
 
 api_bp = Blueprint('api', __name__)
 api = Api(api_bp)
+# TODO: find a cleverer way to define paths?
+leaderboard = LeaderboardManager(os.path.join(
+    str(Path(os.path.dirname(os.path.abspath(__file__))).parents[0]), 'leaderboard.json'))
 
 
 @api_bp.route('/')
@@ -14,4 +20,4 @@ def route_api():
 
 
 # register resources
-api.add_resource(add, '/add')
+api.add_resource(add, '/add', resource_class_kwargs={'leaderboard': leaderboard})
--- views/resources/add.py
+++ views/resources/add.py
@@ -4,15 +4,22 @@ import sys
 from functools import wraps
 from pathlib import Path
 
-from flask_restful import Resource, request
+from flask_restful import Resource, reqparse, request
 
 from ...users import UserManager
-
-user_manager = UserManager(os.path.join(
-    str(Path(os.path.dirname(os.path.abspath(__file__))).parents[1]), "users.json"))
+from ...leaderboard import LeaderboardManager
 
 
 class add(Resource):
+
+    # TODO: find a cleverer way to define paths?
+    user_manager = UserManager(os.path.join(
+        str(Path(os.path.dirname(os.path.abspath(__file__))).parents[1]), 'users.json'))
+    leaderboard: LeaderboardManager
+
+    def __init__(self, leaderboard: LeaderboardManager):
+        self.leaderboard = leaderboard
+
     def check(self, authorization_header: str) -> bool:
         """ Checks the authorization header using the user manager. Returns true
         if the user + password in the header is valid, false otherwise.
@@ -22,13 +29,28 @@ class add(Resource):
         decoded_username_password = base64.b64decode(
             authorization_header.split()[-1].encode('ascii')).decode('ascii')
         username_password_split = decoded_username_password.split(':')
-        return user_manager.validate_user(username_password_split[0], username_password_split[1])
+        return self.user_manager.validate_user(username_password_split[0], username_password_split[1])
 
     def post(self):
         authorization_header = request.headers.get('Authorization')
         if authorization_header and self.check(authorization_header):
-            return {'message': 'Authorized oida'}
+            return self.process_request()
 
         # authorization failed
         # abort, user is not authenticated
         return {'message': 'You need to be authorized to perform this action.'}, 401
+
+    def process_request(self) -> dict:
+        """Processes an incoming request to this resource by an authorized user, returns
+        the response for this request."""
+
+        parser = reqparse.RequestParser()
+        parser.add_argument('name', type=str, required=True) # the player's (user)name
+        parser.add_argument('score', type=int, required=True) # the player's reached score
+        parser.add_argument('message', type=str) # players can append an optional message displayed on screen
+
+        args = parser.parse_args()
+        self.leaderboard.add(args['name'], args['score'], args['message'] if args['message'] else "")
+        # TODO: send change to front end
+
+        return {'message': 'Successfully added new leaderboard entry.'}
