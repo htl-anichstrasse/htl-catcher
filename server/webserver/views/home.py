fc38372aa5a143755da9b020a14d189e39c2faf0
Bemoty <jos.winkler@tsn.at>
2020-01-15 09:37:47 +0100

    remove home functions

--- server/webserver/views/home.py
+++ server/webserver/views/home.py
@@ -10,15 +10,3 @@ home = Blueprint('home', __name__)
 def route_home():
     """ Renders the default page of the webserver, the leaderboard display"""
     return render_template("home.html", data=leaderboard_manager.get_sorted_data())
-
-
-def add_entry(leaderboard_manager: LeaderboardManager):
-    """ Fired when a new entry is added via the /add REST node"""
-    # TODO: Implement method
-    pass
-
-
-def remove_entry(leaderboard_manager: LeaderboardManager):
-    """ Fired when a entry is removed from the leaderboard via the /remove REST node"""
-    # TODO: Implement method
-    pass
