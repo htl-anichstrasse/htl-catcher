b67fcab81fa1e12795447f348c8704a6142128ec
Bemoty <jos.winkler@tsn.at>
2020-01-29 16:41:04 +0100

    module stuff

--- server/webserver/__init__.py
+++ server/webserver/__init__.py
@@ -1,9 +1,14 @@
 from pathlib import Path
 
+from flask import Flask
+
 from webserver.util import LeaderboardManager
 
 from .config import *
 from .definitions import *
+
+# initialize flask app
+app = Flask(__name__)
 
 leaderboard_manager = LeaderboardManager(
     Path(os.path.join(ROOT_DIR, './static/leaderboard.json')))
