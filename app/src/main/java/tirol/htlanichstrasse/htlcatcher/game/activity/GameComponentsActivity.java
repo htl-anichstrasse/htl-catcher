4e1c00d6654311171e264c89246f75eb1bcb0d99
StillRosi <nirossi@tsn.at>
2020-02-02 20:44:48 +0100

    game componentes activity

---
+++ app/src/main/java/tirol/htlanichstrasse/htlcatcher/game/activity/GameComponentsActivity.java
@@ -0,0 +1,26 @@
+package tirol.htlanichstrasse.htlcatcher.game.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+
+/**
+ * @author Nicolaus Rossi
+ * @since 02.02.2020
+ */
+
+public class GameComponentsActivity extends AppCompatActivity implements OnClickListener {
+
+   @Override
+   protected void onCreate(@Nullable Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+   }
+
+   @Override
+   public void onClick(final View view) {
+
+   }
+}
