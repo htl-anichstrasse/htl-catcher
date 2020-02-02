25a7c04348b2daf566261e9b76a7bfc35d5e88fd
StillRosi <nirossi@tsn.at>
2020-02-02 18:00:03 +0100

    added multiplier logo

---
+++ app/src/main/java/tirol/htlanichstrasse/htlcatcher/game/component/MultiplierLogo.java
@@ -0,0 +1,31 @@
+package tirol.htlanichstrasse.htlcatcher.game.component;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * A subtype of the normal logo which grants a point-multiplier for a certain duration.
+ *
+ * @author Nicolaus Rossi
+ * @since 02.02.2020
+ */
+
+@Getter
+@Setter
+public class MultiplierLogo extends Logo {
+
+   /**
+    * Determines the duration of the multiplier.
+    */
+   private long multiplierDuration = 0L;
+
+   /**
+    * Creates a new multiplier-logo on the GameView canvas
+    *
+    * @param x the x coordinate of the logo
+    * @param y the y coordinate of the logo
+    */
+   public MultiplierLogo(final int x, final int y, final int radius) {
+      super(x, y, radius);
+   }
+}
