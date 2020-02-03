f7e8e880ad60fb6f6cc0cba80da3cba4e0a78255
StillRosi <n.rossi2002@gmail.com>
2020-02-03 07:52:05 +0100

    bufferlogo -> shieldlogo

--- app/src/main/java/tirol/htlanichstrasse/htlcatcher/game/stats/GameStatistics.java
+++ app/src/main/java/tirol/htlanichstrasse/htlcatcher/game/stats/GameStatistics.java
@@ -71,7 +71,7 @@ public class GameStatistics {
     * @since 05.11.2019
     */
    public enum StatisticsAction {
-      SECOND(1), OBSTACLE(1), LOGO(10), BONUSLOGO(25), BUFFERLOGO(10)   ;
+      SECOND(1), OBSTACLE(1), LOGO(10), BONUSLOGO(25), SHIELDLOGO(10)   ;
 
 
       /**
