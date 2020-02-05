4a4d2455ab8032e8ab34570c9a4b6fcdb81c2adb
Bemoty <jos.winkler@tsn.at>
2020-02-05 17:31:54 +0100

    add setup flask cli command

--- server/webserver/__main__.py
+++ server/webserver/__main__.py
@@ -1,11 +1,12 @@
 import sys
 
+import click
 from flask import Flask
+from flask.cli import with_appcontext
 
 from webserver.definitions import ROOT_DIR
-from webserver.views import api_bp, home
-
 from webserver.models import db
+from webserver.views import api_bp, home
 
 # fix import
 sys.path.insert(0, ROOT_DIR)
@@ -19,8 +20,14 @@ def page_not_found(error):
     """ Handles 404 errors by simply just returning the code and no page rendering"""
     return "", 404
 
+# database setup command
+@click.command(name="create_tables")
+@with_appcontext
+def create_tables():
+    db.create_all()
 
-# start the app if in main module
+
+    # start the app if in main module
 if __name__ == '__main__':
     # register blueprints
     app.register_blueprint(home)
@@ -28,6 +35,9 @@ if __name__ == '__main__':
 
     # set config
     app.config.from_pyfile('config.py', silent=True)
+
+    # register setup command
+    app.cli.add_command(create_tables)
 
     # initialize postgres
     db.init_app(app)
