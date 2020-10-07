709f326626cfee2ee82d42732a7f1e7f0c48bfac
Bemoty <jos.winkler@tsn.at>
2020-10-07 08:37:31 +0200

    turn this thing round

--- app/src/main/java/tirol/htlanichstrasse/htlcatcher/util/CatcherConfig.java
+++ app/src/main/java/tirol/htlanichstrasse/htlcatcher/util/CatcherConfig.java
@@ -45,7 +45,7 @@ public class CatcherConfig {
    /**
     * The configured password of the webserver for basic auth
     */
-   private String APIPassword = "XXX";
+   private String APIPassword = "yXf9M4LSPN49DebAXhnwYezCY";
 
    /*
       LOGO CONFIGURATION
